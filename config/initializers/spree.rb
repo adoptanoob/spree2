c50919356e8cadb150ccaec809d9283a719c9ff6
adoptanoob <adoptanoobinrails@gmail.com>
2014-09-26 11:18:50 +0200

    added precompile whitelist

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,3 +1,11 @@
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( bx_loader.gif )
+Rails.application.config.assets.precompile << %w( store/print.css )
+Rails.application.config.assets.precompile << %w( icons.svg icons.ttf icons.eot icons.woff )
+Rails.application.config.assets.precompile << %w( images/bx_loader.gif images/controls.png )
+
+
+
 # Configure Spree Preferences
 #
 # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.

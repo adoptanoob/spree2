f2b5e5b36754c3e1a577a891b4f00a11ecf6c876
adoptanoob <adoptanoobinrails@gmail.com>
2014-09-26 11:29:00 +0200

    relocated last tweak in right initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bx_loader.gif )
+Rails.application.config.assets.precompile << %w( store/print.css )
+Rails.application.config.assets.precompile << %w( icons.svg icons.ttf icons.eot icons.woff )
+Rails.application.config.assets.precompile << %w( images/bx_loader.gif images/controls.png )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )

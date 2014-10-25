c6fa0ff241430a69caa9567c5d7d5b263168de6d
adoptanoob <adoptanoobinrails@gmail.com>
2014-10-25 13:01:03 +0200

    new config for images and paperclip storage in s3

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,11 +14,7 @@ Rails.application.config.assets.precompile += %w( images/bx_loader.gif images/co
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  config.s3_bucket = ENV['AWS_S3_BUCKET']
-  config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
-  config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
-  config.attachment_url = ":s3_eu_url"
-  config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
+
 end
 
 Spree.user_class = "Spree::User"
